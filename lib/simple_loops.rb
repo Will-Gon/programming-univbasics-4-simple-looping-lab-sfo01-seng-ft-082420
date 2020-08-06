04c9a2f0fe6731b315fac20d9b7982c3d56a9dcc
Will Gonzalez <will.colocho@gmail.com>
2020-08-06 05:26:55 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -3,7 +3,7 @@ def loop_message_five_times(message)
   while count < 5 do 
     puts message 
     count += 1 
-end
+  end
 end 
 
 def loop_message_n_times(message, n)
@@ -11,5 +11,13 @@ def loop_message_n_times(message, n)
   while count < n do 
     puts message 
     count += 1 
-end
+  end
 end 
+
+def output_array(array)
+  count = 0 
+  while count < array.length do 
+    puts array[count]
+    count += 1 
+  end
+end
